6c9b89eb5721f67826d23e62f3f6563629d4d14a
Piscki Pratama <pisckipratama@gmail.com>
2020-03-31 17:08:43 +0700

    setting gmt 7

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var moment = require('moment');
+var moment = require('moment-timezone');
 moment.locale('id');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', {
     title: 'Data Covid-19',
-    jam: moment().format('LLLL')
+    jam: moment.tz("Asia/Jakarta").format('LLLL')
   });
 });
 
